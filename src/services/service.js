aa89643b59b79969c11fdba430b0dee0f56456c9
Thomas campo <thomascampocood@gmail.com>
2023-08-02 23:41:17 -0300

    repository

--- src/services/service.js
+++ src/services/service.js
@@ -1,11 +1,11 @@
 import UserManager from "../dao/mongo/manager/UserManagerMongo.js";
-import UserService from "./user.service.js";
+import UserService from "./repository/user.service.js";
 
 import CartManager from "../dao/mongo/manager/CartManagerMongo.js";
-import CartService from "./cart.service.js";
+import CartService from "./repository/cart.service.js";
 
 import TicketManager from "../dao/mongo/manager/TicketManagerMongo.js";
-import TicketService from "./ticket.service.js";
+import TicketService from "./repository/ticket.service.js";
 
 export const userService = new UserService(new UserManager());
 
